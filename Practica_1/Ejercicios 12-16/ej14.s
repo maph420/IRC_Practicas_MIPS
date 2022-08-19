fd9b456dc385d481732007fa14a3f769cfce314a
Juan Ignacio Bertoni <71228707+maph420@users.noreply.github.com>
2022-08-19 16:56:14 -0300

    ex 14

---
+++ Practica_1/Ejercicios 12-16/ej14.s
@@ -0,0 +1,8 @@
+.data 0x10000000
+palabra: .word 0xabcd12bd
+.text
+main:
+lw $t1, palabra
+
+# 0xDD7FFFFF es un numero binario con todos los bits a 1, menos los bits 3,7,9
+andi $t1, $t1, 0xDD7FFFFF
