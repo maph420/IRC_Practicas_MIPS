c58c9dfadd85da0cb6c496e68453773fd7dfe5f1
Juan Ignacio Bertoni <71228707+maph420@users.noreply.github.com>
2022-08-19 16:45:59 -0300

    ex 13

--- Practica_1/Ejercicios 12-16/ej13.s
+++ Practica_1/Ejercicios 12-16/ej13.s
@@ -1,25 +1,21 @@
-.data 
-num1: .byte 18
-num2: .byte -1215
-divisor: .byte 5
-divnums: .space 2
+.data 0x1000000
+num1: .word 18
+num2: .word -1215
+divisor: .word 5
 
-divDeDivs: .byte 0x10010000
+divDeDivs: .word 0x10010000
+
 .text
 main:
 
-la $t4, divnums
+lw $t0, num1
+lw $t1, num2
+lw $t2, divisor
 
-lb $t0, num1
-lb $t1, num2
-lb $t2, divisor
+div $t4, $t0, $t2
+div $t5, $t1, $t2
 
-la $t3, divnums
+div $t6, $t5, $t4
 
-div $t3, $t0, $t2
-sb $t3, 0($t4)
-
-addi $t3, $t3, 1
-
-div $t3, $t1, $t2
+sw $t6, divDeDivs
 
