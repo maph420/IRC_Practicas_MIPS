a721224e11a28e9e06d1cafc29fbd523f9654760
maph <juani.bertoni73@gmail.com>
2022-11-05 22:21:15 -0300

    only item 5 is left

--- Practica_3/trabajo_cobamips3.asm
+++ Practica_3/trabajo_cobamips3.asm
@@ -20,13 +20,20 @@ noCatsMessage: 			.asciiz "No hay categorias creadas actualmente\n"
 showNextCatMessage: 		.asciiz "La siguiente categoria es: \n"
 showCatsMessage: 		.asciiz "Las categorias son:\n"
 endLoopMessage:  		.asciiz "Loop is no more"
-asterisk:			.asciiz "* "
+asterisk:			.asciiz "(*)"
 changeCatMessage:		.asciiz "Te moviste hacia la categoria: "
 deleteCatMessage:		.asciiz "Se ha eliminado la categoria "
+newObjMessage:			.asciiz "Ingresar nombre del objeto a crear: \n"
+showObjsMessage:		.asciiz "Los objetos relacionados a la categoria son:\n"
+noObjectsMessage:		.asciiz "No hay objetos en la lista \n"
+inputIDMessage:			.asciiz "Ingresar ID del objeto a borrar \n"
+space:				.asciiz " "
+rmObjMessage:			.asciiz "Se elimino el objeto "
+
 
 slist:          .word 0 #lista simplemente enlazada para la memoria
-cclist:         .word 0 #lista de categorias
-wclist:         .word 0 #lista de elementos
+cclist:         .word 0 # category list
+wclist:         .word 0 # working category list
  
                 .text
 main:
@@ -76,10 +83,10 @@ derivar_a_funcion:
                 beq $t0, 2, siguientecategoria2
                 beq $t0, 3, categoriaanterior3
                 beq $t0, 4, traverseList4
-                beq $t0, 5, deleteCategory5
-                # beq $t0, 6, appendObject6
-                # beq $t0, 7, deleteObject7
-                # beq $t0, 8, showObject8
+                # beq $t0, 5, deleteCategory5
+                beq $t0, 6, appendObject6
+                beq $t0, 7, deleteObject7
+                beq $t0, 8, showObject8
                 beq $t0, 9, fin
  
                 j mainMenu
@@ -151,24 +158,6 @@ nonEmpty:       sw $v0, cclist # si no es vacio guarda cclist en $v0
                 jr $ra
 
  
-# $a0 is the pointer to the first field of the node
-# Frees memory for node given its pointer
-delnode:
-                addiu $sp, $sp, -4         # guarda en el stack la direccion para volver
-                sw $ra, 4($sp)
-
-                lw $t0, ($a0)                   # (t0 )previous node (| | | | |) <- |x| | | |
-                lw $t1, 12($a0)                 # (t1) next node | | | | | . | | | |x| -> (| | | | |)
-                sw $t0, ($t1)                   # store prv node into prv of nxt | | | | | . |x| | | | . |x| | | |
-                addiu $t0, $t0, 12
-                sw $t1, ($t0)                   # store nxt node into nxt of prv | | | |x| . | | | |x| . | | | | |
-
-                jal sfree            # libera la memoria del nodo en $a0
-
-                lw $ra, 4($sp)
-                addiu $sp, $sp, 4
-                jr $ra
- 
  
 # $a0: string name
 # $v0: returns node address
@@ -212,48 +201,51 @@ empty:          # si la lista es vacia, la dir del nodo ant y del post es la mis
 
 
 siguientecategoria2:
-		# cargar lista de categorias en $t0
-		lw $t0, cclist
+		# cargar lista de la categoria actual en $t2
+		lw $t2, wclist
 		
 		beqz $t0 noCats
 		
 		# desplazar el valor de $t0 a la dir del siguiente
-		lw $t0, 12($t0)
-		sw $t0, cclist
-		
-		# cargar en $t1 la direccion de la tercer pos del nodo (el nombre de la categoria)
+		lw $t2, 12($t2)
+		sw $t2, wclist
+		sw $t2, cclist
 		
 		la $a0, changeCatMessage
 		jal print
 		
-		lw $t1, 8($t0)
+		# cargar en $t1 la direccion de la tercer pos del nodo (el nombre de la categoria)
+		lw $t1, 8($t2)
 		move $a0, $t1
 		jal print
 		j menu
 
 categoriaanterior3:
-		# cargar lista de categorias en $t0
-		lw $t0, cclist
+
+		# cargar lista de la categoria actual en $t2
+		lw $t2, cclist
 		
 		beqz $t0 noCats
 		
 		# desplazar el valor de $t0 a la dir del anterior
-		lw $t0, 0($t0)
-		sw $t0, cclist
+		lw $t2, 0($t2)
+		sw $t2, wclist
+		sw $t2, cclist
 		
-		# cargar en $t1 la direccion de la tercer pos del nodo (el nombre de la categoria)
 		
 		la $a0, changeCatMessage
 		jal print
 		
-		lw $t1, 8($t0)
+		# cargar en $t1 la direccion de la tercer pos del nodo (el nombre de la categoria)
+		lw $t1, 8($t2)
 		move $a0, $t1
 		jal print
 		j menu
 
 traverseList4:
-		# cargar lista de categorias en $t0
+		# cargar lista de la categoria actual en $t0
 		lw $t0, cclist
+		# cargo dir del nodo previo en $t3
 		la $t3, 0($t0)
 		
 		beqz $t0 noCats
@@ -261,11 +253,8 @@ traverseList4:
 		la $a0, showCatsMessage
 		jal print
 		
-		
-		lw $t1, 8($t0)
-		
-		
 		# hace lo mismo del loop solo que agregandole el *
+		lw $t1, 8($t0)
 		la $a0, asterisk
 		jal print
 		move $a0, $t1
@@ -298,9 +287,219 @@ endLoop:
 
 
 deleteCategory5:
+		la $a0, deleteCatMessage
+		jal print
+		
+		# guardar cclist en $t0
+		lw $a0, cclist
+		
+		lw $t1, 8($a0)
+		move $a0, $t1
+		jal print
+		
+		lw $a0, cclist
+		jal delnode
 		
 		j menu
 
+
+appendObject6:
+		# cargar lista de cat actual en $t5
+		lw $t5, wclist
+		# cargar direccion a lista de objetos de la cat actual en $t1
+		lw $t1, 4($t5)
+		
+		
+		# mostrar mensaje ingresar nombre
+		la $a0, newObjMessage
+		jal print
+		
+		# ingresar nombre objeto
+		jal smalloc
+                move $a0, $v0
+                li $a1, 16
+                
+                # guardar en $a0 el nombre
+                li $v0, 8
+                syscall
+		
+		# si aun no hay una lista de objetos creada para esa categoria, crearla
+		beqz $t1, emptyObjList
+		
+		# guardar id del anterior al objeto actual en la lista
+		lw $t7, 0($t1)
+		lw $t6, 4($t7)
+		
+		# toma el string $a0 para crear el nuevo nodo y retorna su direccion en $v0
+		jal newnode
+		
+		# cargar wclist en $a0
+		move $a0, $t1
+		# cargar memoria reservada en $a1
+		move $a1, $v0
+		
+		# toma la lista $a0 y su memoria reservada $a1 para agregar el nodo
+		jal addnode
+		
+		addi $t6, $t6, 1
+		lw $t7, 0($v0)
+		sw $t6, 4($t7)
+		
+		lw $t7, wclist
+		sw $v0, 4($t7)
+		j menu
+
+emptyObjList:
+		# toma el string $a0 para crear el nuevo nodo y retorna su direccion en $v0
+		jal newnode
+		
+		# mueve valor de la lista de objetos al argumento $a0
+		move $a0, $t1
+		# cargar memoria reservada por newnode en $a1
+		move $a1, $v0
+		
+		# toma la lista $a0 y su memoria reservada $a1 para agregar el nodo
+		jal addnode
+		
+		# cargar la nueva lista en t2
+		move $t2, $v0
+		# $t3=1
+		addi $t3, $zero, 1
+		# la id del unico objeto de la lista es 1 (default)
+		sw $t3, 4($t2)
+		
+		lw $t7, wclist
+		sw $v0, 4($t7)
+		j menu
+
+deleteObject7:
+		# mensaje de ingresar ID
+		la $a0, inputIDMessage
+		jal print
+		
+		# Lee un entero
+                li $v0, 5
+                syscall
+                move $t0, $v0 # carga el input en t0
+		
+		lw $t1, wclist
+		lw $t6, 0($t2)
+		
+		# cargar en $t2 lista de objetos
+		lw $t2, 4($t1)
+		
+		beq $t0, $t2, singleElemCase2
+		# si el anterior es igual al mismo, significa que hay un unico nodo en la lista
+		# en ese caso hay que encargarse ademas de poner a 0 (null) el segundo campo de categoria
+		#beq $t6, $t2, singleElemCase1
+		
+		
+		
+		# cargar en $t6 la dir del nodo anterior al primer nodo
+		lw $t6, 0($t2)
+		lw $t7, 12($t2)
+		lw $t8, wclist
+			
+		j lookForID
+lookForID:
+		# cargar id asignada al nodo actual en $t3
+		lw $t3, 4($t2)
+		
+		beq $t3, $t0, deleteNodeWithID
+		
+		lw $t2, 12($t2) # ahora $t2 apunta al siguiente nodo
+		j lookForID
+		
+		j menu
+		
+deleteNodeWithID:
+		la $a0, rmObjMessage
+		jal print
+		
+		lw $t6, 8($t2)
+		move $a0, $t6
+		jal print
+		
+		move $a0, $t2
+		jal delnode
+		
+		j menu
+
+# estoy eliminando el primer elemento, y este es el unico
+singleElemCase1:
+		addi $t6, $zero, 0
+		sw $t6, 4($t1)
+		move $a0, $t2
+		jal delnode
+		j menu
+
+# estoy eliminando el primer elemento, sin ser este el unico		
+singleElemCase2:
+		# cargar en $t6 la dir del nodo anterior al primer nodo
+		lw $t6, 0($t2)
+		
+
+		j loop3
+# recorro hasta el ultimo nodo de la lista
+loop3:
+		beq $t2, $t6, break
+		lw $t2, 12($t2)
+		j loop3
+
+break:
+		sw $t7, 12($t2)
+		sw $t2, 0($t7)
+		sw $t7, wclist
+		j menu
+
+showObject8:
+		# cargar lista de categorias en $t0
+		lw $t0, wclist
+		
+		beqz $t0 noCats
+		
+		la $a0, showObjsMessage
+		jal print
+		
+		# cargar en $t2 lista de objetos
+		lw $t2, 4($t0)
+		
+		beqz $t2, emptyobj
+		
+		# cargar en $t6 la dir del nodo anterior al primer nodo
+		lw $t6, 0($t2)
+			
+		j loop2
+loop2:
+		# cargar nombre asignado al nodo actual en $t1
+		lw $t1, 8($t2)
+		
+		# mostrar id del nodo actual
+		lw $t7, 4($t2)
+		move $a0, $t7
+		jal printint
+		
+		# espacio entre ID y nombre
+		la $a0, space
+		jal print
+		
+		
+		# mostrar objeto del nodo actual
+		move $a0, $t1
+		jal print
+		
+		beq $t6, $t2, endLoop2
+		#beq $t2, $t3, endLoop2
+		lw $t2, 12($t2) # ahora $t2 apunta al siguiente nodo
+		j loop2
+
+endLoop2:
+		j menu
+
+emptyobj:
+		la $a0, noObjectsMessage
+		jal print
+		j menu
 
 
 # imprime, tomando como argumento $a0
@@ -308,6 +507,31 @@ print:
 	li	$v0, 4
 	syscall
 	jr	$ra
+
+# imprime, tomando como argumento $a0
+printint:
+	li	$v0, 1
+	syscall
+	jr	$ra
+
+
+# $a0 is the pointer to the first field of the node
+# Frees memory for node given its pointer
+delnode:
+                addiu $sp, $sp, -4         # guarda en el stack la direccion para volver
+                sw $ra, 4($sp)
+
+                lw $t0, ($a0)                   # (t0 )previous node (| | | | |) <- |x| | | |
+                lw $t1, 12($a0)                 # (t1) next node | | | | | . | | | |x| -> (| | | | |)
+                sw $t0, ($t1)                   # store prv node into prv of nxt | | | | | . |x| | | | . |x| | | |
+                addiu $t0, $t0, 12
+                sw $t1, ($t0)                   # store nxt node into nxt of prv | | | |x| . | | | |x| . | | | | |
+
+                jal sfree            # libera la memoria del nodo en $a0
+
+                lw $ra, 4($sp)
+                addiu $sp, $sp, 4
+                jr $ra
 
 ### LOW LEVEL FUNCTIONS ###
  
